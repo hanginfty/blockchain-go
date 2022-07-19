38c15c6e900db9233affbee8dd6868f12a0ee697
hanginfty <2263126765@qq.com>
2022-07-20 00:02:16 +0800

    add boltdb support

--- blockchain.go
+++ blockchain.go
@@ -1,15 +1,33 @@
 package main
 
+import (
+	"log"
+
+	"github.com/boltdb/bolt"
+)
+
 type BlockChain struct {
-	blocks []*Block
+	// hash of the latest block.
+	tip []byte
+	db  *bolt.DB
 }
 
 func NewBlockChain() *BlockChain {
-	return &BlockChain{[]*Block{NewGenesis()}}
+	//return &BlockChain{[]*Block{NewGenesis()}}
+
+	var tip []byte
+	db, err := bolt.Open(dbFile, 0600, nil)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	// TODO: db
+
+	return &BlockChain{tip, db}
 }
 
-func (chain *BlockChain) AddBlock(data string) {
-	newestBlock := chain.blocks[len(chain.blocks)-1]
-	newBlock := NewBlock(data, newestBlock.Hash)
-	chain.blocks = append(chain.blocks, newBlock)
-}
+//func (chain *BlockChain) AddBlock(data string) {
+//	newestBlock := chain.blocks[len(chain.blocks)-1]
+//	newBlock := NewBlock(data, newestBlock.Hash)
+//	chain.blocks = append(chain.blocks, newBlock)
+//}
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module blockchain-go
 
 go 1.18
+
+require (
+	github.com/boltdb/bolt v1.3.1 // indirect
+	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+)
